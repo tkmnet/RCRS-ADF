4ab7071f11bcd72e7c8664c1b2b61f60aebc430d
Shunki TAKAMI(takamin@sylph.maslab) <tkmnet@users.noreply.github.com>
2015-11-16 08:46:17 +0900

    update

--- modules/core/src/main/java/adf/agent/info/ScenarioInfo.java
+++ modules/core/src/main/java/adf/agent/info/ScenarioInfo.java
@@ -36,10 +36,133 @@ public class ScenarioInfo
 		return mode;
 	}
 
-	/*
-	public void test()
+	public Config getRawConfig()
 	{
-		config.getIntValue("fire.exthinguish.max-sum");
+		return this.config;
 	}
-	*/
+
+	public int getFireExtinguishMax_Sum()
+	{
+		return config.getIntValue("fire.exthinguish.max-sum");
+	}
+
+	public int getCommsChannelsMaxPlatoon()
+	{
+		return config.getIntValue("comms.channels.max.platoon");
+	}
+
+	public int getKernelAgentsThink_Time()
+	{
+		return config.getIntValue("kernel.agents.think-time ");
+	}
+
+	public int getFireTankMaximum()
+	{
+		return config.getIntValue("fire.tank.maximum ");
+	}
+
+	public int getClearRepairRate()
+	{
+		return config.getIntValue("clear.repair.rate");
+	}
+
+	public int getKernelStartupConnect_Time()
+	{
+		return config.getIntValue("kernel.startup.connect-time");
+	}
+
+	public String getKernelHost()
+	{
+		return config.getValue("kernel.host");
+	}
+
+	public int getScenarioAgentsAt()
+	{
+		return config.getIntValue("scenario.agents.at");
+	}
+
+	public int getPerceptionLosMax_Distance()
+	{
+		return config.getIntValue("perception.los.max-distance");
+	}
+
+	public int getScenarioAgentsFb()
+	{
+		return config.getIntValue("scenario.agents.fb");
+	}
+
+	public int getScenarioAgentsPo()
+	{
+		return config.getIntValue("scenario.agents.po");
+	}
+
+	public String getKernelCommunication_Model()
+	{
+		return config.getValue("kernel.communication-model");
+	}
+
+	public int getPerceptionLosPrecisionDamage()
+	{
+		return config.getIntValue("perception.los.precision.damage");
+	}
+
+	public int getScenarioAgentsAc()
+	{
+		return config.getIntValue("scenario.agents.ac");
+	}
+
+	public int getCommsChannelsMaxOffice() //public int getCommsChannelsMaxCentre()
+	{
+		return config.getIntValue("comms.channels.max.centre");
+	}
+
+	public int getFireExtinguishMax_Distance()
+	{
+		return config.getIntValue("fire.extinguish.max-distance");
+	}
+
+	public int getKernelAgentsIgnoreuntil()
+	{
+		return config.getIntValue("kernel.agents.ignoreuntil");
+	}
+
+	public int getClearRepairDistance()
+	{
+		return config.getIntValue("clear.repair.distance");
+	}
+
+	public int getCommsChannelsCount()
+	{
+		return config.getIntValue("comms.channels.count");
+	}
+
+	public String getKernelPerception()
+	{
+		return config.getValue("kernel.perception");
+	}
+
+	public int getPerceptionLosPrecisionHp()
+	{
+		return config.getIntValue("perception.los.precision.hp");
+	}
+
+	public int getClearRepairRad()
+	{
+		return config.getIntValue("clear.repair.rad");
+	}
+
+	public int getFireTankRefill_Hydrant_Rate()
+	{
+		return config.getIntValue("fire.tank.refill_hydrant_rate");
+	}
+
+	public int getScenarioAgentsPf()
+	{
+		return config.getIntValue("scenario.agents.pf");
+	}
+
+	public int getScenarioAgentsFs()
+	{
+		return config.getIntValue("scenario.agents.fs");
+	}
 }
